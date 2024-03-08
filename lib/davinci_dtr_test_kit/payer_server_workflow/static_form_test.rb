cbf8dc07129131b4b8a4b54bea6ea9f7a1b1eeba
rpassas <rpassas@mitre.org>
2024-03-07 20:31:16 -0500

    updating questionnaire test

--- lib/davinci_dtr_test_kit/payer_server_workflow/static_form_test.rb
+++ lib/davinci_dtr_test_kit/payer_server_workflow/static_form_test.rb
@@ -8,11 +8,11 @@ module DaVinciDTRTestKit
 
     run do
       assert_valid_resource(resource: questionnaire_parameters, profile_url: "http://hl7.org/fhir/us/davinci-dtr/StructureDefinition/dtr-qpackage-input-parameters")
-      fhir_operation("#{url}/Questionnaire/$questionnaire-package", body: questionnaire_parameters)
+      fhir_operation("#{url}/Questionnaire/HomeOxygenTherapyAdditional/$questionnaire-package/", body: JSON.parse(questionnaire_parameters), headers: {"Content-Type": "application/json"})
 
       assert_response_status(200)
-      assert_resource_type(:questionnaire_bundle)
-      assert_valid_resource(profile_url: 'http://hl7.org/fhir/us/davinci-dtr/StructureDefinition/DTR-QPackageBundle')
+      assert_resource_type(:parameters)
+      assert_valid_resource(profile_url: 'http://hl7.org/fhir/us/davinci-dtr/StructureDefinition/dtr-qpackage-output-parameters')
 
       output questionnaire_bundle: questionnaire_bundle
     end
