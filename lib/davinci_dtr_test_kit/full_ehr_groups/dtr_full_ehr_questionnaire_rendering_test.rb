cd6ab821f911163f11081c00c8ffec15431ac277
Tom Strassner <tstrassner@mitre.org>
2024-04-05 08:48:36 -0400

    FI-2618: Modify Full EHR test suite to work with reference implementation

---
+++ lib/davinci_dtr_test_kit/full_ehr_groups/dtr_full_ehr_questionnaire_rendering_test.rb
@@ -0,0 +1,29 @@
+require_relative '../urls'
+
+module DaVinciDTRTestKit
+  class QuestionnaireRenderingTest < Inferno::Test
+    include URLs
+
+    id :dtr_full_ehr_questionnaire_rendering
+    title 'Check that the client renders the questionnaire (Attestation)'
+    description %(
+      Thist test provides the tester an opportunity to observe their client application following the receipt of the
+      questionnaire pacakage and attest that the application renders the questionnaire.
+    )
+    input :access_token
+
+    run do
+      wait(
+        identifier: access_token,
+        message: %(
+          I attest that the client application displays the questionnaire and respects the following rendering style:
+          - The "Signature" field label is rendered with green text
+
+          [Click here](#{resume_pass_url}?token=#{access_token}) if the above statement is **true**.
+
+          [Click here](#{resume_fail_url}?token=#{access_token}) if the above statement is **false**.
+        )
+      )
+    end
+  end
+end
