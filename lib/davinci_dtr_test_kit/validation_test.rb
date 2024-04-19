02d4625c41da7c8fe5fe8589fd79a72260fe430a
rpassas <rpassas@mitre.org>
2024-04-19 10:46:42 -0400

    labeling errors with resource order

--- lib/davinci_dtr_test_kit/validation_test.rb
+++ lib/davinci_dtr_test_kit/validation_test.rb
@@ -1,11 +1,18 @@
 module DaVinciDTRTestKit
   module ValidationTest
 
-    def validate_resource(fhir_resource, resource_type, profile_url)
+    def validate_resource(fhir_resource, resource_type, profile_url, index)
       assert fhir_resource.present?, 'Resource does not contain a recognized FHIR object'
-      assert_resource_type(resource_type, resource: fhir_resource)
-      assert_valid_resource(resource: fhir_resource,
-                            profile_url: profile_url)
+      begin
+        assert_resource_type(resource_type, resource: fhir_resource)
+        assert_valid_resource(resource: fhir_resource,
+                              profile_url: profile_url)
+      rescue StandardError => e
+        messages.each do |message|
+          message[:message].prepend("[Resource #{index+1}] ")
+        end
+        raise e
+      end
     end
 
     def perform_request_validation_test(
@@ -15,12 +22,12 @@ module DaVinciDTRTestKit
       resource_url)
       omit_if resources.blank?,
             "No #{resource_type.to_s} resources provided so the #{profile_url} profile does not apply"
-      resources.each do |resource|
+      resources.each_with_index do |resource, index|
         assert resource.url == resource_url,
           "Request made to wrong URL: #{resource.request[:url]}. Should instead be to #{resource_url}"
         assert_valid_json(resource.request[:body])
         fhir_resource = FHIR.from_contents(resource.request[:body])
-        validate_resource(fhir_resource, resource_type, profile_url)
+        validate_resource(fhir_resource, resource_type, profile_url, index)
       end
     end
 
@@ -28,12 +35,14 @@ module DaVinciDTRTestKit
       resources,
       resource_type,
       profile_url)
+      # require 'debug/open_nonstop'
+      # debugger
       omit_if resources.blank?,
           "No #{resource_type.to_s} resources provided so the #{profile_url} profile does not apply"
-      resources.each do |resource|
+      resources.each_with_index do |resource, index|
         fhir_resource = FHIR.from_contents(resource.response[:body])
         assert_response_status(200, request: resource, response: resource.response)
-        validate_resource(fhir_resource, resource_type, profile_url)
+        validate_resource(fhir_resource, resource_type, profile_url, index)
       end
     end
   end
