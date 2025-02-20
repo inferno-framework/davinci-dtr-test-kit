eaf0884eaeb2e4f61f026c32ee5cf0123c82a6eb
Karl Naden <knaden@mitre.org>
2025-02-20 12:01:24 -0500

    fix wait continue urls

--- lib/davinci_dtr_test_kit/client_groups/dinner_static/dtr_static_dinner_enabled_questions_attestation_test.rb
+++ lib/davinci_dtr_test_kit/client_groups/dinner_static/dtr_static_dinner_enabled_questions_attestation_test.rb
@@ -20,9 +20,9 @@ module DaVinciDTRTestKit
           questions until I have selected a dinner choice and then only displays the
           "What would you like on..." question relevant for the dinner request:
 
-          [Click here](#{resume_pass_url}?client_id=#{random_id}) if the above statement is **true**.
+          [Click here](#{resume_pass_url}?token=#{random_id}) if the above statement is **true**.
 
-          [Click here](#{resume_fail_url}?client_id=#{random_id}) if the above statement is **false**.
+          [Click here](#{resume_fail_url}?token=#{random_id}) if the above statement is **false**.
         )
       )
     end
