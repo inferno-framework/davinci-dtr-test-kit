74e6250cf7dd9f6ede8d6a09dfbcf6611246ef77
Shaumik-Ashraf <shaumikashraf@mitre.org>
2025-02-25 09:52:50 -0500

    Bump version to 0.15.0

--- lib/davinci_dtr_test_kit/version.rb
+++ lib/davinci_dtr_test_kit/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module DaVinciDTRTestKit
-  VERSION = '0.14.2'
-  LAST_UPDATED = '2025-02-04' # TODO: update next release
+  VERSION = '0.15.0'
+  LAST_UPDATED = '2025-02-25'
 end
