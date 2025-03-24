a72f905525e7272ea02df6e7e8dddc397f4ff5d8
Vanessa Fotso <vfotso@mitre.org>
2025-03-24 12:37:55 -0400

    fail wait request if inputs are not valid json or are empty
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

--- lib/davinci_dtr_test_kit/client_groups/adaptive_questionnaire/custom/dtr_smart_app_custom_adaptive_request_test.rb
+++ lib/davinci_dtr_test_kit/client_groups/adaptive_questionnaire/custom/dtr_smart_app_custom_adaptive_request_test.rb
@@ -65,6 +65,22 @@ module DaVinciDTRTestKit
     end
 
     run do
+      assert_valid_json(
+        custom_questionnaire_package_response,
+        'Custom questionnaire package response is not a valid json'
+      )
+      assert_valid_json(custom_next_question_questionnaires, 'Custom next questionnaires input is not a valid json')
+
+      custom_qp = JSON.parse(custom_questionnaire_package_response)
+      custom_questionnaires = JSON.parse(custom_next_question_questionnaires)
+      assert custom_qp.present?, %(
+        Custom questionnaire package response is empty, please provide a custom questionnaire package response
+        for the $questionnaire-package request
+      )
+      assert custom_questionnaires.present?, %(
+        'Custom questionnaires list is empty, please provide a list of Custom Questionnaire resources
+        to include in each $next-question Response.
+      )
       # validate relevant inputs and provide warnings if they are bad
       warning do
         if adaptive_smart_fhir_context
