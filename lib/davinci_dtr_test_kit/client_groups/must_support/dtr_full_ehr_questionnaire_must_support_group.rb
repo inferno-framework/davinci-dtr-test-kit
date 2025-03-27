444f914a9690689924914f895ec348f455ba9c1d
Vanessa Fotso <vfotso@mitre.org>
2025-03-27 12:23:00 -0400

    adaptive workflow draft for must support
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

--- lib/davinci_dtr_test_kit/client_groups/must_support/dtr_full_ehr_questionnaire_must_support_group.rb
+++ lib/davinci_dtr_test_kit/client_groups/must_support/dtr_full_ehr_questionnaire_must_support_group.rb
@@ -1,5 +1,6 @@
 require_relative '../../urls'
 require_relative '../custom_static/dtr_full_ehr_cutom_static_retrieval_group'
+require_relative '../adaptive_questionnaire/custom/dtr_full_ehr_custom_adaptive_retrieval_group'
 require_relative 'dtr_must_support_attestation_test'
 require_relative '../full_ehr/dtr_full_ehr_store_attestation_test'
 
@@ -16,7 +17,7 @@ module DaVinciDTRTestKit
       description %()
       config(
         options: {
-          questionnaire_package_tag: "must_support_#{QUESTIONNAIRE_PACKAGE_TAG}"
+          questionnaire_package_tag: "ms_static_#{QUESTIONNAIRE_PACKAGE_TAG}"
         },
         inputs: {
           custom_questionnaire_package_response: {
@@ -73,25 +74,91 @@ module DaVinciDTRTestKit
     group do
       run_as_group
       title 'Adaptive Questionnaires Element Support'
+      description %()
+      config(
+        options: {
+          form_type: 'adaptive',
+          questionnaire_package_tag: "ms_adaptive_#{QUESTIONNAIRE_PACKAGE_TAG}",
+          next_tag: "custom_ms_adaptive_#{CLIENT_NEXT_TAG}"
+        },
+        inputs: {
+          custom_questionnaire_package_response: {
+            name: 'ms_adaptive_custom_questionnaire_package_response',
+            title: 'Custom Adaptive Questionnaire Package Response JSON for Must Support tests',
+            description: %(
+                Provide a JSON `$questionnaire-package` response (a FHIR Parameters resource).
+                Inferno will return this as the response to the `$questionnaire-package` request.
+                The Questionnaire in the `PackageBundle` **must include all `mustSupport` elements**
+                as defined in the DTR Adaptive Questionnaire profile.
+                Note: Ensure that the questionnaire package has an empty Adaptive Questionnaire.
+              )
 
-      group do
+          },
+          custom_next_question_questionnaires: {
+            name: 'ms_custom_next_question_questionnaires',
+            title: 'Custom Questionnaire resources to include in each $next-question Response for Must Support tests',
+            description: %(
+              Provide a JSON list of Questionnaire resources for Inferno to use when updating
+              the contained Questionnaire in the `QuestionnaireResponse` received in each
+              `$next-question` request.
+
+              Each `$next-question` request will correspond to the next item in the provided list,
+              and Inferno will replace the contained Questionnaire with the corresponding resource
+              before returning the updated `QuestionnaireResponse`.
+
+              The provided Questionnaires must contain the next question or set of questions in
+              sequence, ensuring a proper progression of the adaptive questionnaire workflow.
+              Each Questionnaire **must include all `mustSupport` elements** as defined in the
+              DTR Adaptive Questionnaire profile.
+            ),
+            type: 'textarea'
+          }
+        }
+      )
+
+      group from: :dtr_full_ehr_custom_adaptive_retrieval do
         title 'Retrieving the Adaptive Questionnaires for must support validation'
+        description %(
+          During this test, DTR will be launched within the Full EHR to begin the demonstration of
+          Adaptive Questionnaire capabilities.
+
+          Once DTR is launched, Inferno will wait for client requests to retrieve and progress through
+          the adaptive questionnaire workflow:
+
+          - **Questionnaire Package Request**: The client should invoke the `$questionnaire-package`
+            operation to retrieve the adaptive questionnaire package. Inferno will respond with the
+            user-provided empty adaptive questionnaire.
+
+          - **Next Question Requests**: The client should invoke the `$next-question` operation to
+            request the next set of questions. Inferno will respond sequentially with the next
+            Questionnaire from the user-provided list. If a `$next-question` request is received
+            when the list is empty, Inferno will mark the QuestionnaireResponse as completed.
+
+          Inferno will validate that:
+          - The response to the `$questionnaire-package` operation conforms to the [Questionnaire Package
+            operation definition](https://hl7.org/fhir/us/davinci-dtr/STU2/OperationDefinition-questionnaire-package.html).
+          - Each user-provided Questionnaire resource included in a `$next-question` response is valid
+            and appropriate for the workflow.
+        )
       end
 
       group do
         title '[USER INPUT VALIDATION] Adaptive Questionnaires Must Support'
         test 'All must support elements are provided in the adaptive Questionnaire resources provided' do
+          run { omit 'Not yet implemented' }
         end
       end
 
       group do
         title 'Attestation: Filling Out the Static Questionnaire'
-        test 'Attest rendering and filling of provided adaptive Questionnaire resources' do
-        end
+
+        test from: :dtr_must_support_attest
       end
 
       group do
         title 'Attestation: QuestionnaireResponse Completion and Storage'
+
+        test from: :dtr_full_ehr_store_attest
       end
     end
   end
