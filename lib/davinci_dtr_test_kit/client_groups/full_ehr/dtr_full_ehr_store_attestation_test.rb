f851b4e1591d4876def8e5c15709515f914b859b
Vanessa Fotso <vfotso@mitre.org>
2025-04-08 13:07:23 -0400

    simplify prompt
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

--- lib/davinci_dtr_test_kit/client_groups/full_ehr/dtr_full_ehr_store_attestation_test.rb
+++ lib/davinci_dtr_test_kit/client_groups/full_ehr/dtr_full_ehr_store_attestation_test.rb
@@ -12,30 +12,13 @@ module DaVinciDTRTestKit
       and can be exported as a FHIR QuestionnaireResponse instance.
     )
 
-    def form_type
-      config.options[:form_type]
-    end
-
-    def wait_message
-      if form_type == 'adaptive'
-        <<~DESCRIPTION
-          I attest that the questionnaire has been completed and stored within the EHR for future
-          use and export as a FHIR QuestionnaireResponse instance.
-        DESCRIPTION
-      else
-        <<~DESCRIPTION
-          I attest that all provided questionnaires have been completed and stored within the EHR for future
-          use and export as FHIR QuestionnaireResponse instances.
-        DESCRIPTION
-      end
-    end
-
     run do
       random_id = SecureRandom.uuid
       wait(
         identifier: random_id,
         message: <<~MESSAGE
-          #{wait_message}
+          I attest that questionnaires have been completed and stored within the EHR for future
+          use and export as a FHIR QuestionnaireResponse instance.
 
           [Click here](#{resume_pass_url}?token=#{random_id}) if the above statement is **true**.
 
