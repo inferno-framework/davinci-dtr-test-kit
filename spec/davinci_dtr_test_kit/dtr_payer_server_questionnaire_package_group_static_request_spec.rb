0898d2686ac45f68c1d5eb4d1abd68d7b949eebd
Shaumik-Ashraf <shaumikashraf@mitre.org>
2025-06-17 10:45:20 -0400

    fix questionnaire package group static request spec

--- spec/davinci_dtr_test_kit/dtr_payer_server_questionnaire_package_group_static_request_spec.rb
+++ spec/davinci_dtr_test_kit/dtr_payer_server_questionnaire_package_group_static_request_spec.rb
@@ -1,37 +1,32 @@
-require_relative 'shared_setup'
-
 RSpec.describe DaVinciDTRTestKit::DTRPayerServerQuestionnairePackageGroup, :request do
   let(:suite_id) { 'dtr_payer_server' }
-
-  include_context('when running standard tests',
-                  'payer_server_static_package', # group
-                  suite_id = :dtr_payer_server,
-                  "/custom/#{suite_id}/fhir/Questionnaire/$questionnaire-package", # questionnaire_package_url
-                  'Static', # retrieval_method
-                  'http://example.org/fhir/R4') # url
+  let(:results_repo) { Inferno::Repositories::Results.new }
+  let(:url) { 'https://example.com/fhir/R4' }
+  let(:access_token) { 'dummy' }
 
   context 'when initial request/response is manually provided' do
+    let(:retrieval_method) { 'Static' }
     let(:initial_static_questionnaire_request) do
       File.read(File.join(__dir__, '..', 'fixtures', 'questionnaire_package_input_params_conformant.json'))
     end
 
     describe 'static questionnaire package incoming request test' do
-      let(:runnable) { group.tests.find { |test| test.id.to_s.end_with? 'request_test' } }
+      let(:runnable) { find_test described_class, 'request_test' }
 
       it 'passes if valid questionnaire request is manually provided' do
-        result = run(runnable, test_session, access_token:,
-                                             retrieval_method:, initial_static_questionnaire_request:, url:)
+        result = run(runnable, { url:, access_token:, retrieval_method:, initial_static_questionnaire_request: })
         expect(result.result).to eq('pass'), result.result_message
       end
 
       it 'skips if access token is nil' do
-        result = run(runnable, test_session, retrieval_method:, initial_static_questionnaire_request:, url:)
+        result = run(runnable, { url:, retrieval_method: })
         expect(result.result).to eq('skip'), result.result_message
       end
     end
 
     describe 'static Questionnaire request validation' do
-      let(:runnable) { group.tests.find { |test| test.id.to_s.end_with? 'static_form_request_validation_test' } }
+      let(:runnable) { find_test described_class, 'static_form_request_validation_test' }
+      let(:retrieval_method) { 'Static' }
       let(:input_validation_test) do
         Class.new(Inferno::Test) do
           def self.suite
@@ -63,12 +58,12 @@ RSpec.describe DaVinciDTRTestKit::DTRPayerServerQuestionnairePackageGroup, :requ
                 })
           .to_return(status: 200, body: FHIR::OperationOutcome.new.to_json)
 
-        result = run(runnable, test_session, access_token:, retrieval_method:, initial_static_questionnaire_request:)
+        result = run(runnable, { access_token:, retrieval_method:, initial_static_questionnaire_request: })
         expect(result.result).to eq('pass'), result.result_message
       end
 
       describe 'invalid request passed manually' do
-        let(:initial_static_questionnaire_request) do
+        let(:bad_static_questionnaire_request) do
           File.read(File.join(__dir__, '..', 'fixtures', 'questionnaire_package_input_params_non_conformant.json'))
         end
 
@@ -77,8 +72,8 @@ RSpec.describe DaVinciDTRTestKit::DTRPayerServerQuestionnairePackageGroup, :requ
         end
 
         it 'skips if questionnaire request is not conformant' do
-          result = run(input_validation_test, test_session, access_token:, retrieval_method:,
-                                                            initial_static_questionnaire_request:)
+          result = run(input_validation_test,
+                       { url:, access_token:, retrieval_method:, initial_static_questionnaire_request: bad_static_questionnaire_request })
           expect(result.result).to eq('skip'), result.result_message
         end
       end
@@ -87,23 +82,23 @@ RSpec.describe DaVinciDTRTestKit::DTRPayerServerQuestionnairePackageGroup, :requ
     # all static tests should skip when flow is marked 'adaptive'
     context 'when retrieval method is adaptive' do
       let(:retrieval_method) { 'Adaptive' }
+      let(:inputs_adaptive_method) { inputs.merge(retrieval_method:) }
 
       describe 'static questionnaire package incoming request test' do
-        let(:runnable) { group.tests.find { |test| test.id.to_s.end_with? 'request_test' } }
+        let(:runnable) { find_test described_class, 'request_test' }
         let(:results_repo) { Inferno::Repositories::Results.new }
 
         it 'skips when retrieval method is adaptive' do
-          result = run(runnable, test_session, access_token:, retrieval_method:, url:)
+          result = run(runnable, { url:, access_token:, retrieval_method: })
           expect(result.result).to eq('skip')
         end
       end
 
       describe 'static questionnaire package request validation test' do
-        let(:runnable) { group.tests.find { |test| test.id.to_s.end_with? 'static_form_request_validation_test' } }
-        let(:results_repo) { Inferno::Repositories::Results.new }
+        let(:runnable) { find_test described_class, 'static_form_request_validation_test' }
 
         it 'skips when retrieval method is adaptive' do
-          result = run(runnable, test_session, access_token:, retrieval_method:, initial_static_questionnaire_request:)
+          result = run(runnable, { access_token:, retrieval_method: })
           expect(result.result).to eq('skip')
         end
       end
@@ -112,16 +107,15 @@ RSpec.describe DaVinciDTRTestKit::DTRPayerServerQuestionnairePackageGroup, :requ
 
   # When initial request is not provided i.e. client flow
   describe 'static questionnaire package incoming request test' do
-    let(:runnable) { group.tests.find { |test| test.id.to_s.end_with? 'request_test' } }
-    let(:results_repo) { Inferno::Repositories::Results.new }
+    let(:runnable) { find_test described_class, 'request_test' }
+    let(:retrieval_method) { 'Static' }
+    let(:resume_pass_url) { "/custom/#{suite_id}/resume_pass?token=#{access_token}" }
     let(:request_body_conformant) do
       File.read(File.join(__dir__, '..', 'fixtures', 'questionnaire_package_input_params_conformant.json'))
     end
     let(:request_body_non_conformant) do
       File.read(File.join(__dir__, '..', 'fixtures', 'questionnaire_package_input_params_non_conformant.json'))
     end
-
-    let(:resume_pass_url) { "/custom/#{suite_id}/resume_pass?token=#{access_token}" }
 
     it 'passes if questionnaire package request is received' do
       stub_request(:post, "#{url}/Questionnaire/$questionnaire-package")
@@ -131,7 +125,7 @@ RSpec.describe DaVinciDTRTestKit::DTRPayerServerQuestionnairePackageGroup, :requ
       allow_any_instance_of(DaVinciDTRTestKit::URLs).to(receive(:fhir_base_url).and_return(''))
       allow_any_instance_of(DaVinciDTRTestKit::URLs).to(receive(:resume_pass_url).and_return(''))
 
-      result = run(runnable, test_session, access_token:, retrieval_method:, url:)
+      result = run(runnable, { url:, access_token:, retrieval_method: })
       expect(result.result).to eq('wait')
 
       header 'Authorization', "Bearer #{access_token}"
@@ -143,11 +137,11 @@ RSpec.describe DaVinciDTRTestKit::DTRPayerServerQuestionnairePackageGroup, :requ
     end
 
     describe 'static questionnaire package request validation test' do
-      let(:runnable) { group.tests.find { |test| test.id.to_s.end_with? 'static_form_request_validation_test' } }
-      let(:results_repo) { Inferno::Repositories::Results.new }
-
+      let(:runnable) { find_test described_class, 'static_form_request_validation_test' }
+      let(:retrieval_method) { 'Static' }
+      
       it 'skips when access_token is nil' do
-        result = run(runnable, test_session, retrieval_method:)
+        result = run(runnable, { url:, retrieval_method: })
         expect(result.result).to eq('skip'), result.result_message
       end
 
@@ -162,7 +156,7 @@ RSpec.describe DaVinciDTRTestKit::DTRPayerServerQuestionnairePackageGroup, :requ
                               tags: [DaVinciDTRTestKit::QUESTIONNAIRE_TAG])
 
         allow_any_instance_of(runnable).to receive(:resource_is_valid?).and_return(true)
-        result = run(runnable, test_session, access_token:, retrieval_method:)
+        result = run(runnable, { url:, access_token:, retrieval_method: })
         expect(result.result).to eq('pass'), result.result_message
       end
 
@@ -186,7 +180,7 @@ RSpec.describe DaVinciDTRTestKit::DTRPayerServerQuestionnairePackageGroup, :requ
                 })
           .to_return(status: 200, body: FHIR::OperationOutcome.new(issue: { severity: 'error' }).to_json)
 
-        result = run(runnable, test_session, access_token:, retrieval_method:)
+        result = run(runnable, { url:, access_token:, retrieval_method: })
         expect(result.result).to eq('skip'), result.result_message
       end
     end
