9d56a38b041796cfd38cc41b2c5d61bba29744ca
Stephen MacVicar <smacvicar@mitre.org>
2026-08-06 10:18:56 -0400

    add spec for cql library inclusion

---
+++ spec/davinci_dtr_test_kit/server/v2.2.0/cql_library_validation_test_spec.rb
@@ -0,0 +1,237 @@
+RSpec.describe DaVinciDTRTestKit::DTRPayerServerV220::CQLLibraryValidationTest do # rubocop:disable RSpec/SpecFilePathFormat
+  let(:suite_id) { 'dtr_payer_server_v220' }
+  let(:results_repo) { Inferno::Repositories::Results.new }
+  let(:library_url) { 'http://example.com/fhir/library' }
+  let(:library_version) { '0.1' }
+  let(:library_canonical) { "#{library_url}|#{library_version}" }
+  let(:q_with_lib_ref) do
+    FHIR::Questionnaire.new(
+      extension: [
+        {
+          url: 'http://hl7.org/fhir/StructureDefinition/cqf-library',
+          valueCanonical: library_canonical
+        }
+      ]
+    )
+  end
+
+  def build_bundle(questionnaire, libraries = [])
+    bundle_hash = {
+      entry: [
+        {
+          resource: questionnaire
+        }
+      ]
+    }
+
+    libraries.each do |library|
+      bundle_hash[:entry] << { resource: library }
+    end
+
+    FHIR::Bundle.new(bundle_hash)
+  end
+
+  def response_params(bundle)
+    FHIR::Parameters.new(
+      parameter: [
+        {
+          name: 'packagebundle',
+          resource: bundle
+        }
+      ]
+    )
+  end
+
+  def result_messages
+    results_repo
+      .current_results_for_test_session_and_runnables(test_session.id, [described_class])
+      .first&.messages || []
+  end
+
+  it 'skips if no requests have been made' do
+    result = run(described_class)
+
+    expect(result.result).to eq('skip')
+    expect(result.result_message).to include('No successful $questionnaire-package')
+  end
+
+  it 'skips if no successful requests have been made' do
+    request = repo_create(:request, status: 400, response_body: '{}')
+
+    allow_any_instance_of(described_class).to receive(:requests).and_return([request])
+
+    result = run(described_class)
+
+    expect(result.result).to eq('skip')
+    expect(result.result_message).to include('No successful $questionnaire-package')
+  end
+
+  it 'omits if no responses contain libraries' do
+    bundle = build_bundle(FHIR::Questionnaire.new)
+    request = repo_create(:request, status: 200, response_body: response_params(bundle).to_json)
+
+    allow_any_instance_of(described_class).to receive(:requests).and_return([request])
+
+    result = run(described_class)
+
+    expect(result.result).to eq('omit')
+  end
+
+  it 'fails if a library does not contain cql' do
+    library = FHIR::Library.new(
+      url: library_url,
+      version: library_version,
+      content: [
+        {
+          contentType: 'application/elm+json',
+          data: 'abc'
+        }
+      ]
+    )
+    bundle = build_bundle(q_with_lib_ref, [library])
+    request = repo_create(:request, status: 200, response_body: response_params(bundle).to_json)
+
+    allow_any_instance_of(described_class).to receive(:requests).and_return([request])
+
+    result = run(described_class)
+
+    expect(result.result).to eq('fail')
+    expect(result_messages.first.message).to include('does not include CQL')
+  end
+
+  it 'fails if a library does not contain elm' do
+    library = FHIR::Library.new(
+      url: library_url,
+      version: library_version,
+      content: [
+        {
+          contentType: 'text/cql',
+          data: 'abc'
+        }
+      ]
+    )
+    bundle = build_bundle(q_with_lib_ref, [library])
+    request = repo_create(:request, status: 200, response_body: response_params(bundle).to_json)
+
+    allow_any_instance_of(described_class).to receive(:requests).and_return([request])
+
+    result = run(described_class)
+
+    expect(result.result).to eq('fail')
+    expect(result_messages.first.message).to include('does not include ELM')
+  end
+
+  it 'fails if multiple libraries have the same name' do
+    library1 = FHIR::Library.new(
+      url: library_url,
+      name: 'abc',
+      version: library_version,
+      content: [
+        {
+          contentType: 'text/cql',
+          data: 'abc'
+        },
+        {
+          contentType: 'application/elm+json',
+          data: 'abc'
+        }
+      ]
+    )
+    library2 = library1.dup
+    library2.url = "#{library1.url}x"
+
+    bundle = build_bundle(q_with_lib_ref, [library1, library2])
+    request = repo_create(:request, status: 200, response_body: response_params(bundle).to_json)
+
+    allow_any_instance_of(described_class).to receive(:requests).and_return([request])
+
+    result = run(described_class)
+
+    expect(result.result).to eq('fail')
+    expect(result_messages.first.message).to include('contains multiple libraries named')
+  end
+
+  it 'fails if a referenced library is not included' do
+    library = FHIR::Library.new(
+      url: library_url,
+      name: 'abc',
+      version: "#{library_version}.1",
+      content: [
+        {
+          contentType: 'text/cql',
+          data: 'abc'
+        },
+        {
+          contentType: 'application/elm+json',
+          data: 'abc'
+        }
+      ]
+    )
+
+    bundle = build_bundle(q_with_lib_ref, [library])
+    request = repo_create(:request, status: 200, response_body: response_params(bundle).to_json)
+
+    allow_any_instance_of(described_class).to receive(:requests).and_return([request])
+
+    result = run(described_class)
+
+    expect(result.result).to eq('fail')
+    expect(result_messages.first.message).to include('libraries are referenced but not included')
+  end
+
+  it 'fails if a non-versioned library reference is present' do
+    q_with_lib_ref.extension.first.valueCanonical = library_url
+    library = FHIR::Library.new(
+      url: library_url,
+      name: 'abc',
+      version: library_version,
+      content: [
+        {
+          contentType: 'text/cql',
+          data: 'abc'
+        },
+        {
+          contentType: 'application/elm+json',
+          data: 'abc'
+        }
+      ]
+    )
+
+    bundle = build_bundle(q_with_lib_ref, [library])
+    request = repo_create(:request, status: 200, response_body: response_params(bundle).to_json)
+
+    allow_any_instance_of(described_class).to receive(:requests).and_return([request])
+
+    result = run(described_class)
+
+    expect(result.result).to eq('fail')
+    expect(result_messages.first.message).to include('unversioned library references')
+  end
+
+  it 'passes if all referenced libraries are included and valid' do
+    library = FHIR::Library.new(
+      url: library_url,
+      name: 'abc',
+      version: library_version,
+      content: [
+        {
+          contentType: 'text/cql',
+          data: 'abc'
+        },
+        {
+          contentType: 'application/elm+json',
+          data: 'abc'
+        }
+      ]
+    )
+
+    bundle = build_bundle(q_with_lib_ref, [library])
+    request = repo_create(:request, status: 200, response_body: response_params(bundle).to_json)
+
+    allow_any_instance_of(described_class).to receive(:requests).and_return([request])
+
+    result = run(described_class)
+
+    expect(result.result).to eq('pass')
+  end
+end
