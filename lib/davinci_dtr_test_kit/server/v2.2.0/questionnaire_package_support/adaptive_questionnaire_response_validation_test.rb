fb2c2eae43db332200e1e739d65a735ab7ff9d62
Tony Dao <tonydao1@bu.edu>
2026-08-12 10:37:39 -0400

    Added generated requirements coverage files

--- lib/davinci_dtr_test_kit/server/v2.2.0/questionnaire_package_support/adaptive_questionnaire_response_validation_test.rb
+++ lib/davinci_dtr_test_kit/server/v2.2.0/questionnaire_package_support/adaptive_questionnaire_response_validation_test.rb
@@ -1,6 +1,6 @@
 require_relative '../../../tags'
 require_relative '../../validation_test'
-require_relative '../../../cross_suite/v2.2.0/questionnaire_operation_validation'
+require_relative '../questionnaire_operation_validation'
 
 module DaVinciDTRTestKit
   module DTRPayerServerV220
