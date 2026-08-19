04b003f6118fbba322ccc51486cd1deb5b8c9e31
Gunst <298841834+annagunst@users.noreply.github.com>
2026-08-19 13:45:23 -0400

    Removed input, revised error handling

--- lib/davinci_dtr_test_kit/server/v2.2.0/questionnaire_package_support/dtr_server_questionnaire_not_found_test.rb
+++ lib/davinci_dtr_test_kit/server/v2.2.0/questionnaire_package_support/dtr_server_questionnaire_not_found_test.rb
@@ -1,48 +1,53 @@
+require_relative '../../../tags'
+
 module DaVinciDTRTestKit
   module DTRPayerServerV220
     class DTRServerQuestionnaireNotFoundTest < Inferno::Test
       id :dtr_server_v220_payer_questionnaire_not_found
       title 'Questionnaire package response includes a warning for an unknown Questionnaire'
       description %(
-        The DTR Questionnaire Package operation [requires](https://hl7.org/fhir/us/davinci-dtr/2.2.0/en/confdetails.html#ci-c-oper-9)
-        requires the server to return a warning when a requested Questionnaire cannot be found.
-        Inferno sends a tester-provided `$questionnaire-package` request that includes an unknown Questionnaire
-        and verifies that the response contains the required warning.
+        The DTR Questionnaire Package operation requires the server to return a warning
+        when a requested Questionnaire cannot be found. Inferno adds an unknown Questionnaire
+        to a previously made `$questionnaire-package` request and verifies that the
+        response contains the required warning.
       )
 
       verifies_requirements 'hl7.fhir.us.davinci-dtr_2.2.0@oper-9'
 
+      QUESTIONNAIRE_NOT_FOUND_URL = 'https://inferno.health/Questionnaire/does-not-exist'.freeze
+
       input :url,
             title: 'Payer FHIR Server Base Url',
             description: 'Base FHIR URL implementing the DTR server operations.'
-
-      input :questionnaire_not_found_request,
-            title: 'Questionnaire Package Request with Unknown Questionnaire',
-            description: %(
-              Provide a `Parameters` resource in JSON format for a
-              `Questionnaire/$questionnaire-package` request that explicitly requests
-              at least one Questionnaire known not to exist on the payer server.
-            ),
-            type: 'textarea'
 
       fhir_client do
         url :url
       end
 
       run do
-        request = FHIR.from_contents(questionnaire_not_found_request)
+        load_tagged_requests(QUESTIONNAIRE_TAG)
+
+        skip_if requests.blank?,
+                'No $questionnaire-package requests were made in the Request Questionnaires test.'
+
+        request = FHIR.from_contents(requests.first.request_body)
 
         assert_resource_type(:parameters, resource: request)
+
+        request.parameter << FHIR::Parameters::Parameter.new(
+          name: 'questionnaire',
+          valueCanonical: QUESTIONNAIRE_NOT_FOUND_URL
+        )
 
         fhir_operation(
           '/Questionnaire/$questionnaire-package',
           body: request
         )
 
-        assert_response_status(200)
         assert_resource_type(:parameters)
+        assert_valid_resource
 
-        outcome_parameter = Array(resource.parameter).find do |parameter|
+        outcome_parameter = resource.parameter.find do |parameter|
           parameter.name == 'outcome'
         end
 
@@ -56,7 +61,7 @@ module DaVinciDTRTestKit
 
         assert_resource_type(:operation_outcome, resource: outcome)
 
-        has_warning = Array(outcome.issue).any? do |issue|
+        has_warning = outcome.issue.any? do |issue|
           issue.severity == 'warning'
         end
 
