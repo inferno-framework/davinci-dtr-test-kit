99364df38c8ec5f17d0ddb3e237933f6aeb9d615
Gunst <298841834+annagunst@users.noreply.github.com>
2026-08-24 10:20:44 -0400

    Removed Oper-12 comment lines

--- lib/davinci_dtr_test_kit/server/v2.2.0/dtr_payer_server_suite.rb
+++ lib/davinci_dtr_test_kit/server/v2.2.0/dtr_payer_server_suite.rb
@@ -137,13 +137,17 @@ module DaVinciDTRTestKit
 
         group do
           title 'Questionnaire/$questionnaire-package Support'
-          # oper-12 - [NOT TESTED in 2.0] include Questionnaire as 1st entry, [TESTED] and CQL libraries
-          test from: :dtr_server_v220_payer_questionnaire_package_contents
+
+          # TODO: verify that each of these parameter types is used
+
+          # One with CRD/PAS context ID
+          # Request/Encounter resource
 
           # oper-14 - [NOT TESTED in 2.0] Bundle includes all VS instances
           # oper-16 - [NOT TESTED in 2.0] references are version specific
           #           NOTE: ONLY Library references are currently tested
           test from: :dtr_v220_payer_questionnaire_response_validation
+          test from: :dtr_server_v220_payer_questionnaire_package_contents
 
           # TODO: embedded QR validation
           # spec-25 - [NOT TESTED in 2.0] QR has contained Q
