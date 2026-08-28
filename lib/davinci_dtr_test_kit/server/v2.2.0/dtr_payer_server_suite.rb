6518d7c4780920b0aa19833ff63b03bfbd5da1e3
bryantranv <75497594+bryantranv@users.noreply.github.com>
2026-08-28 11:38:33 -0400

    FQM-451: Verify contained Questionnaire presence (#116)
    * FQM-451: Verify contained Questionnaire presence
    
    * FQM-451: Update spec-122 test to omit adaptive questionnaires
    
    * Update lib/davinci_dtr_test_kit/server/v2.2.0/questionnaire_package_support/questionnaire_response_questionnaire_canonical_test.rb
    
    Co-authored-by: Stephen MacVicar <Jammjammjamm@users.noreply.github.com>
    
    * FQM-451: Update spec-122 expectations for for omitted standard Qs
    
    ---------
    
    Co-authored-by: Stephen MacVicar <Jammjammjamm@users.noreply.github.com>

--- lib/davinci_dtr_test_kit/server/v2.2.0/dtr_payer_server_suite.rb
+++ lib/davinci_dtr_test_kit/server/v2.2.0/dtr_payer_server_suite.rb
@@ -20,6 +20,7 @@ require_relative 'next_question_support/next_question_response_references_test'
 require_relative 'next_question_support/next_contained_questionnaire_response_references_test'
 require_relative 'next_question_support/adaptive_questionnaire_endpoint_test'
 require_relative 'questionnaire_package_support/adaptive_questionnaire_response_validation_test'
+require_relative 'questionnaire_package_support/adaptive_questionnaire_response_contained_questionnaire_test'
 require_relative 'value_set_expand_support/value_set_expansion_test'
 require_relative 'questionnaire_design/questionnaire_expression_elm_test'
 require_relative 'questionnaire_design/questionnaire_expression_language_test'
@@ -151,8 +152,7 @@ module DaVinciDTRTestKit
           test from: :dtr_v220_payer_value_set_validation
           test from: :dtr_v220_payer_questionnaire_references_version_specific
 
-          # TODO: embedded QR validation
-          # spec-25 - [NOT TESTED in 2.0] QR has contained Q
+          test from: :dtr_v220_payer_adaptive_questionnaire_response_contained_questionnaire
           test from: :dtr_v220_payer_questionnaire_response_questionnaire_canonical
           test from: :dtr_v220_payer_questionnaire_response_references
           test from: :dtr_v220_payer_contained_questionnaire_response_references
