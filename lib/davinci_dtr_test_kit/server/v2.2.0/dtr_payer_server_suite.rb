c88fa64ad2102ea1b50145ce03dbec4e9ce0a5bb
Tony Dao <123415277+tonydao2@users.noreply.github.com>
2026-09-03 10:45:11 -0400

    FQM-456: Verify small VS are expanded (#121)
    * implemented oper-15
    
    * fixed merge conf for coverage
    
    * refactored test for small ValueSet to read from questionnaire-package response
    
    * Fix ValueSet expansion validation
    
    * changed naming to url instead of canonical
    
    * Changed method name and added unit tests for code only
    
    * removed redundancy
    
    * merge conf
    
    * regenerated coverage

--- lib/davinci_dtr_test_kit/server/v2.2.0/dtr_payer_server_suite.rb
+++ lib/davinci_dtr_test_kit/server/v2.2.0/dtr_payer_server_suite.rb
@@ -23,6 +23,7 @@ require_relative 'next_question_support/adaptive_questionnaire_endpoint_test'
 require_relative 'questionnaire_package_support/adaptive_questionnaire_response_validation_test'
 require_relative 'questionnaire_package_support/adaptive_questionnaire_response_contained_questionnaire_test'
 require_relative 'value_set_expand_support/value_set_expansion_test'
+require_relative 'value_set_expand_support/small_value_set_expansion_test'
 require_relative 'questionnaire_design/questionnaire_expression_elm_test'
 require_relative 'questionnaire_design/questionnaire_expression_language_test'
 require_relative 'questionnaire_design/questionnaire_prepopulation_test'
@@ -198,6 +199,7 @@ module DaVinciDTRTestKit
           test from: :dtr_v220_payer_initial_questionnaire_response
           test from: :dtr_server_v220_no_custom_extension_test
           test from: :dtr_v220_payer_value_set_validation
+          test from: :dtr_v220_payer_small_value_set_expansion
           test from: :dtr_v220_payer_questionnaire_references_version_specific
 
           test from: :dtr_v220_payer_adaptive_questionnaire_response_contained_questionnaire
@@ -265,9 +267,6 @@ module DaVinciDTRTestKit
           DESCRIPTION
 
           test from: :dtr_v220_payer_value_set_expansion
-
-          # TODO
-          # oper-15 - [NOT TESTED in 2.0] VS with <40 entries SHALL be expanded
         end
 
         group do
